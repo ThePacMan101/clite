01e9323d4288a82f0a837332597fc3a329566452
Lucas Pimentel <lulalucas.27@gmail.com>
2026-04-21 13:12:09 -0300

    feat: implements constant values

--- src/main.c
+++ src/main.c
@@ -7,7 +7,10 @@
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     init_chunk(&chunk);
-    write_chunk(&chunk,OP_RETURN);
+    int constant = add_constant(&chunk,4.2);
+    write_chunk(&chunk,OP_CONSTANT,12);
+    write_chunk(&chunk,constant,12);
+    write_chunk(&chunk,OP_RETURN,12);
 
     disassemble_chunk(&chunk,"test chunk");
     free_chunk(&chunk);
---
+++ src/value.h
@@ -0,0 +1,20 @@
+#ifndef CLITE_VALUE_H
+#define CLITE_VALUE_H
+
+#include "common.h"
+#include <stdio.h>
+
+typedef double Value;
+
+typedef struct {
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+
+void init_value_array(ValueArray* array);
+void write_value_array(ValueArray* array,Value value);
+void free_value_array(ValueArray* array);
+void print_value(Value value);
+
+#endif
